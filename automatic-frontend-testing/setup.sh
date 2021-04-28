51d23cf74c3629c79078aa76822a190cf6b81c68
Markus Wesslén <markus.wesslen@gmail.com>
2021-04-28 15:39:16 +0200

    Modify setup script

--- automatic-frontend-testing/setup.sh
+++ automatic-frontend-testing/setup.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-/usr/bin/python3.7 -m pip install --upgrade pip
-ln -sf /usr/bin/python3.7 /usr/bin/python
-ln -sf /usr/local/bin/pip3.7 /usr/local/bin/pip
+/usr/bin/python3 -m pip install --upgrade pip
